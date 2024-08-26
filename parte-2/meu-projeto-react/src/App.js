6fbfebc72d3911e2e43f4eca4bcfda568a8406b9
ibonfim <indaiara.bonfim@jusbrasil.com.br>
2024-08-26 08:19:42 -0300

    subindo alterações no front

--- parte-2/meu-projeto-react/src/App.js
+++ parte-2/meu-projeto-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
     sobrecarga_sistema: '',
     paginas: ''
   });
-  
+  const [selectedScheduler, setSelectedScheduler] = useState(1); // 1 for FIFO, 2 for Round Robin, 3 for EDF, 4 for SJF
+
   useEffect(() => {
-    // Busca a lista de processos do backend quando o componente é montado
     axios.get('http://localhost:8000/getprocesslist')
       .then(response => {
         setProcessList(response.data.process);
@@ -24,22 +24,16 @@ function App() {
       });
   }, []);
 
-   
   const handleChange = (e) => {
     const { name, value } = e.target;
-    
     setNewProcess({
       ...newProcess,
-      [name]: value 
-      
-       
+      [name]: value
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Converte o campo 'paginas' de string para array de números
     const processData = {
       ...newProcess,
       paginas: newProcess.paginas.split(',').map(Number)
@@ -47,14 +41,11 @@ function App() {
 
     axios.post('http://localhost:8000/newprocess', processData)
       .then(response => {
-        // Atualiza o estado com a lista de processos recebida
         setProcessList(response.data.process);
-        // Limpa o formulário
         setNewProcess({
           tempo_chegada: '',
           tempo_execucao: '',
           deadline: '',
-          tempo_restante: '',
           quantum_sistema: '',
           sobrecarga_sistema: '',
           paginas: ''
@@ -66,10 +57,9 @@ function App() {
   };
 
   const createGraph = () => {
-    axios.post('http://localhost:8000/creategraph')
+    axios.post('http://localhost:8000/creategraph', { tipo_escalonador: selectedScheduler })
       .then(response => {
-        // Atualiza o estado com a imagem recebida
-        setImage(`data:image/png;base64,${response.data.image}`);
+        setImage(`data:text/html;base64,${btoa(response.data.html)}`);
       })
       .catch(error => {
         console.error('There was an error creating the graph!', error);
@@ -79,7 +69,7 @@ function App() {
   return (
     <div className="App">
       <h1>Process Scheduler</h1>
-      
+
       <form onSubmit={handleSubmit}>
         <h2>Add New Process</h2>
         <div>
@@ -104,7 +94,6 @@ function App() {
             />
           </label>
         </div>
-        
         <div>
           <label>Deadline:
             <input
@@ -116,43 +105,30 @@ function App() {
             />
           </label>
         </div>
-        {processList.length === 0 && (
-        <>
-          <div>
-            <label>Quantum Sistema:
-              <input
-                type="number"
-                name="quantum_sistema"
-                value={newProcess.quantum_sistema}
-                onChange={handleChange}
-                required
-              />
-            </label>
-          </div>
-          <div>
-            <label>Sobrecarga Sistema:
-              <input
-                type="number"
-                name="sobrecarga_sistema"
-                value={newProcess.sobrecarga_sistema }
-                onChange={handleChange}
-                required
-              />
-            </label>
-          </div>
-        </>
-      )}
-      {/* <div>
-          <label>Tempo restante:
-            <input
-              type="number"
-              name="tempo_restante"
-              value={newProcess.tempo_restante}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>*/}
+        
+            <div>
+              <label>Quantum Sistema:
+                <input
+                  type="number"
+                  name="quantum_sistema"
+                  value={newProcess.quantum_sistema}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+            <div>
+              <label>Sobrecarga Sistema:
+                <input
+                  type="number"
+                  name="sobrecarga_sistema"
+                  value={newProcess.sobrecarga_sistema}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </div>
+       
         <div>
           <label>Páginas (separadas por vírgula):
             <input
@@ -166,16 +142,23 @@ function App() {
         </div>
         <button type="submit">Add Process</button>
       </form>
+      <div>
+        <h2>Select Scheduler</h2>
+        <button onClick={() => setSelectedScheduler(1)}>FIFO</button>
+        <button onClick={() => setSelectedScheduler(3)}>Round Robin</button>
+        <button onClick={() => setSelectedScheduler(4)}>EDF</button>
+        <button onClick={() => setSelectedScheduler(2)}>SJF</button>
+      </div>
 
       <button onClick={createGraph}>Create Graph</button>
-      {image && <img src={image} alt="Process Graph" />}
-      
+      {image && <iframe src={image} width="1200" height="800" title="Process Graph" />}
+
       <div>
         <h2>Process List</h2>
         <ul>
           {processList.map((process) => (
             <li key={process.id}>
-              ID: {process.id}, Tempo Chegada: {process.tempo_chegada}, Tempo Execução: {process.tempo_execucao}, Deadline: {process.deadline}, Sobrecarga:{process.sobrecarga_sistema}, Quantum:{process.quantum_sistema}
+              ID: {process.id}, Tempo Chegada: {process.tempo_chegada}, Tempo Execução: {process.tempo_execucao}, Deadline: {process.deadline}, Sobrecarga: {process.sobrecarga_sistema}, Quantum: {process.quantum_sistema}
             </li>
           ))}
         </ul>
@@ -183,5 +166,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
